838ec571f32ac4e6a6e44c62ddc19020226ba682
Israel Gilyadov <israelg99@gmail.com>
2015-06-14 16:44:02 -0500

    Improved CSS and couple of other things

--- theme_designed/footer.php
+++
@@ -1,8 +0,0 @@
-		<div id="footer">
-			<div id="copyright" class="low-redglow">
-				Copyright 2015 <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>, All rights reserved.
-			</div>
-		</div>
-	</div>
-</body>
-</html>
